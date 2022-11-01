bf488460a6802ae133e3eb1b98852d5b21afee87
Stephen Cott <stephen.cott@expeditors.com>
2022-11-01 14:27:24 -0700

    Small changes to completion

--- lisp/setup-completion.el
+++ lisp/setup-completion.el
@@ -1,29 +1,39 @@
+;;; Package -- My UI and theme customizations
+;;; Commentary:
+;;; Code:
 
-
+(eval-when-compile (require 'use-package))
+		   
 ;; Install and setup Orderless
-
 (use-package orderless
   :custom
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion)))))
 
 ;; Install and configure Vertico
-
 (use-package vertico
-  :straight (:host github :repo "minad/vertico"
-		   :includes (vertico-directory)
-		   :files (:defaults "extensions/vertico-directory.el"))
+;;  :straight (:host github :repo "minad/vertico"
+;;  :includes (vertico-directory)
+;;  :files (:defaults "extensions/vertico-directory.el"))
   :init
   (vertico-mode))
 
+(use-package savehist
+  :init
+  (savehist-mode))
 
-(use-package vertico-directory
-  :after vertico
-  :bind (:map vertico-map
-	      ("DEL" . vertico-directory-delete-char)
-	      ("M-DEL" . vertico-directory-delete-word))
-  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+;; (use-package vertico-directory
+;;   :after vertico
+;;   :bind (:map vertico-map
+;; 	      ("DEL" . vertico-directory-delete-char)
+;; 	      ("M-DEL" . vertico-directory-delete-word))
+;;   :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
 
-(use-package consult)
+(use-package consult
+  :bind
+  (("C-x b" . consult-buffer)
+   ("M-y" . consult-yank-pop)))
 
-(provide 'setup-completion)
+ (provide 'setup-completion)
+
+;;; setup-completion.el ends here
