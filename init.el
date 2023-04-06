e10357a4916eddb038a1e7ec142aa53dbadf8b0e
Stephen Cott <stephen.cott@expeditors.com>
2023-04-06 08:56:14 -0700

    Removing auto delete of temp files

--- init.el
+++ init.el
@@ -61,15 +61,15 @@
       `((".*" ,temporary-file-directory)))
 
 ;;; Clean up old backup files
-(message "Deleting old backup files...")
-(let ((week (* 60 60 24 7))
-      (current (float-time (current-time))))
-  (dolist (file (directory-files temporary-file-directory t))
-    (when (and (backup-file-name-p file)
-               (> (- current (float-time (fifth (file-attributes file))))
-                  week))
-      (message "%s" file)
-      (delete-file file))))
+;; (message "Deleting old backup files...")
+;; (let ((week (* 60 60 24 7))
+;;       (current (float-time (current-time))))
+;;   (dolist (file (directory-files temporary-file-directory t))
+;;     (when (and (backup-file-name-p file)
+;;                (> (- current (float-time (fifth (file-attributes file))))
+;;                   week))
+;;       (message "%s" file)
+;;       (delete-file file))))
 
 ;; Set up some sane scrolling
 (setq scroll-step 1
