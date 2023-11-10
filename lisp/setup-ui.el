9fa4eea3db1e0a3bbebbeb89c0958e79f32048bc
Stephen Cott <stephen.cott@expeditors.com>
2023-11-10 11:51:14 -0800

    Updating fonts

--- lisp/setup-ui.el
+++ lisp/setup-ui.el
@@ -6,10 +6,12 @@
 
 ;; Set up fonts
 
-(set-face-attribute 'default nil :family "RobotoMono Nerd Font" :height 125)
+;;(set-face-attribute 'default nil :family "RobotoMono Nerd Font" :height 125)
+(set-face-attribute 'default nil :family "Monaspace Neon" :height 125)
 ;;(set-face-attribute 'default nil :family "Inconsolata" :height 115 :weight 'Regular)
 ;;(set-face-attribute 'default nil :family "Iosevka" :height 110)
-(set-face-attribute 'variable-pitch nil :family "Source Sans Pro" :height 120)
+;;(set-face-attribute 'variable-pitch nil :family "Source Sans Pro" :height 120)
+(set-face-attribute 'variable-pitch nil :family "Monaspace Radon" :height 125)
 ;;(set-face-attribute 'variable-pitch nil :family "iA Writer Quattro V" :weight 'Regular :height 110)
 (set-face-attribute 'fixed-pitch nil :family "RobotoMono Nerd Font" :height 110)
 ;;(set-face-attribute 'fixed-pitch nil :family "Iosevka Extended" :height 105 :weight 'regular)
